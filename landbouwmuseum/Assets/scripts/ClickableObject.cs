5a3108a6f89dd18be91c7101f025bb5cc4d23f51
Poeiesz <51441266+Poeiesz@users.noreply.github.com>
2019-11-21 14:21:02 +0100

    koe script & terrein

---
+++ landbouwmuseum/Assets/scripts/ClickableObject.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickableObject : MonoBehaviour
+{
+    public GameObject[] objects;
+
+    int id = 0;
+
+    public string type;
+
+    public void ChangeObject()
+    {
+        id++;
+        switch (type) {
+            case "Cow":
+                switch (id)
+                {
+                    case 1:
+                        objects[0].SetActive(false);
+                        break;
+                    case 2:
+                        objects[0].SetActive(true);
+                        id = 0;
+                        break;
+                }
+                break;
+            case "Chicken":
+                switch (id)
+                {
+                    case 1:
+                        objects[0].SetActive(false);
+                        break;
+                    case 2:
+                        objects[0].SetActive(true);
+                        id = 0;
+                        break;
+                }
+                break;
+        }
+    }
+}
---
+++ landbouwmuseum/Assets/scripts/ObjectClickerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectClickerScript : MonoBehaviour
+{
+    
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if (hit.transform != null)
+                {
+                    hit.transform.GetComponent<ClickableObject>().ChangeObject();
+                }
+            }
+        }
+    }
+
+    private void PrintName(GameObject go)
+    {
+        print(go.name);
+    }
+
+    private void ObjectDisable(GameObject go)
+    {
+  //      cowSkin.SetActive(false);
+    }
+}
